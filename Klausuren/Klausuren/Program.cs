56ed37952b4c5867ea3aafa99bfff6e8e09c5aee
T2brozz <42860763+T2brozz@users.noreply.github.com>
2021-10-15 21:34:36 +0200

    add uebung01 and klausur19/20

---
+++ Klausuren/Klausuren/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Klausuren
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aufgabe01(3);
+        }
+
+        static void Aufgabe01(int eingabe)
+        {
+            char eingabeString = Convert.ToChar(Convert.ToString(eingabe));
+            Console.WriteLine(Convert.ToString(eingabeString));
+            int anzahl = 0;
+            for (int i = 100; i < 1000; i++)
+            {
+                if (i % eingabe == 0)
+                {
+                    anzahl++;
+                }
+                else
+                {
+                    string iString = Convert.ToString(i);
+                
+                    for (int j = 0; j < 2; j++)
+                    {
+                        if (iString[j] == eingabeString)
+                        {
+                            Console.WriteLine(iString);
+                            anzahl++;
+                            break;
+                        }
+                    }
+                }
+            }
+
+
+            Console.WriteLine(anzahl);
+        }
+    }
+}
---
+++ Übung/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Übung01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("was ist dein Name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("wie viel geld möchtest du ausgeben?");
+            double money = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Hast du Zeit?");
+            if (!GetBoolInput())
+            {
+                Environment.Exit(1);
+            }
+
+            Console.WriteLine("Hast du Hunger?");
+            if (GetBoolInput())
+            {
+                if (money < 5)
+                {
+                    Console.WriteLine("Besuch deine ELtern");
+                }
+                else if (money > 4 && money < 15)
+                {
+                    Console.WriteLine("Ess ein Döner?");
+                }
+                else if (money > 15)
+                {
+                    Console.WriteLine($"Studiere die Speisekarte{name}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Hast du langeweile");
+                if (GetBoolInput())
+                {
+                    if (money < 15)
+                    {
+                        Console.WriteLine($"Reicht für nichts {name}");
+
+                    }else
+                    {
+                     Console.WriteLine("was willst du machen?");
+                     string whatDo = Console.ReadLine();
+                     Console.WriteLine($"Los GEhts! {name}! . JEtzt {whatDo}");
+                    }
+                }
+                
+            }
+        }
+
+        static bool GetBoolInput()
+        {
+            char input = Console.ReadKey().KeyChar;
+            if (input == 'y')
+            {
+                return true;
+            }
+            else if (input == 'n')
+            {
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("Falsche EIngabe");
+                Environment.Exit(1);
+            }
+
+            return true;
+        }
+    }
+}
