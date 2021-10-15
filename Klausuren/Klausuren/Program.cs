564605c7550604540c96ee169a085496369d252f
T2brozz <42860763+T2brozz@users.noreply.github.com>
2021-10-15 22:08:36 +0200

    add Prüfungsaufgabe CS 13 2

--- Klausuren/Klausuren/Program.cs
+++ Klausuren/Klausuren/Program.cs
@@ -6,10 +6,11 @@ namespace Klausuren
     {
         static void Main(string[] args)
         {
-            Aufgabe01(3);
+            //Aufgabe19_20_1(3);
+            Aufgabe13_3("10");
         }
 
-        static void Aufgabe01(int eingabe)
+        static void Aufgabe19_20_1(int eingabe)
         {
             char eingabeString = Convert.ToChar(Convert.ToString(eingabe));
             Console.WriteLine(Convert.ToString(eingabeString));
@@ -23,7 +24,7 @@ namespace Klausuren
                 else
                 {
                     string iString = Convert.ToString(i);
-                
+
                     for (int j = 0; j < 2; j++)
                     {
                         if (iString[j] == eingabeString)
@@ -38,6 +39,39 @@ namespace Klausuren
 
 
             Console.WriteLine(anzahl);
+        }
+
+
+        static void Aufgabe13_3(string binary)
+        {
+            static int twoPow(int pow)
+            {
+                int twoTimes = 1;
+                for (int i = 0; i < pow; i++)
+                {
+                    twoTimes *= 2;
+                }
+
+                return twoTimes;
+            }
+
+            int dezimal = 0;
+            for (int i = 0; i < binary.Length; i++)
+            {
+                if (binary[i] == '1')
+                {
+                    dezimal += twoPow(binary.Length - 1 - i);
+                }
+                else if (binary[i] == '0')
+                {
+                }
+                else
+                {
+                    Environment.Exit(-1);
+                }
+            }
+
+            Console.WriteLine(dezimal);
         }
     }
 }
