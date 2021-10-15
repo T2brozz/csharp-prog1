52b30df2bc17ead4b23af6ed67e8e0f5cdb3d03d
T2brozz <42860763+T2brozz@users.noreply.github.com>
2021-10-15 21:30:35 +0200

    add praktikum 01

---
+++ Praktikum1/Praktikum1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Praktikum1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bitte Wert für den Radius eingeben: ");
+            double radius=Convert.ToDouble(Console.ReadLine());
+            double circleArea = Math.Pow(radius,2) * Math.PI;
+            Console.WriteLine("Der Flächeninhalt beträgt: " + circleArea);
+        }
+    }
+}
