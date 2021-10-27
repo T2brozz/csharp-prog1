475e4b15cc16321e883f14e6e6e4b443a4f0cc4e
T2brozz <42860763+T2brozz@users.noreply.github.com>
2021-10-27 10:03:47 +0200

    add praktikum 03

---
+++ Praktikum03/Praktikum03/Praktikum0301/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Praktikum0301
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Eingabe
+            Console.WriteLine("Geben sei den Min Wert!");
+            int min = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("den Max Wert an");
+            int max = Convert.ToInt32(Console.ReadLine());
+            
+            for (int i = min + 1; i < max; i++)
+            {
+                if (i % 7 == 0) Console.Write(i +" "); //Wenn Zahl / 7 Rest 0 hat -> ausgabe
+            }
+            Console.WriteLine();
+            Console.WriteLine("Geben Sie eine Positive Zahl an");
+            int num = Convert.ToInt32(Convert.ToUInt32(Console.ReadLine())); 
+            //Conver.unit32 um bei negativen zahlen error ruaszuwerfen
+            
+            for (int i = num; i > 0; i--)
+            {
+                string output ="";
+                if ((num - i) % 5 == 0) output += "\n"; //alle 5 zahlen ein newline
+                if (i<10) output+=" "; // leerzeichen wenn einstellig
+                Console.Write($"\t{output}{i}"); //ausgabe
+            }
+        }
+    }
+}
