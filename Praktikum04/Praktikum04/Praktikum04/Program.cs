55d5a732aec7c2d957966d9c16e63cad1343758e
T2brozz <42860763+T2brozz@users.noreply.github.com>
2021-11-09 10:28:45 +0100

    add übung and praktikum04

---
+++ Praktikum04/Praktikum04/Praktikum04/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace Praktikum04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int leftBoarder = 20; // spaces between beginnig of the line and start of street
+            int carPos = 7; // position of car on the street
+            bool retry = true; //bool to breake while loop 
+            int rounds = 0; // count round to dispaly it at the end
+            int changeDirectionTimer = 5; // counter that saves in how many rounds the direction is changing 
+            int direction = 1; //0 = left curve ; 1 = neutal ; 2 = right curve
+
+            while (retry)
+            {
+                if (changeDirectionTimer == 0) //hits 0 after 5 rounds 
+                {
+                    changeDirectionTimer = 5;
+                    do
+                    {
+                        direction = Convert.ToInt32(new Random().Next(3)); //set direction and confert it to integer
+                    } while ((leftBoarder <= 1 && direction == 0) ||
+                             (leftBoarder >= 40 && direction == 2)); //checks if its alreay on the border
+                }
+
+                if (direction == 0) //goes eft
+                {
+                    leftBoarder--;
+                    carPos++; // so car position stays the same if street hits a curve
+                }
+                else if (direction == 2) // goes right
+                {
+                    leftBoarder++;
+                    carPos--;
+                }
+
+                string street = new String(' ', leftBoarder); // space between street and beginning of the line
+                street += "."; // point for left border
+                street += new string(' ', 19);
+                street += "."; // add right border
+                street = street.Insert(carPos + leftBoarder, "X"); // add X for the car
+
+                Console.WriteLine(street); // print street
+                if (Console.KeyAvailable)
+                {
+                    char key = Console.ReadKey(true).KeyChar; //get keychar
+                    if (key == 'a')
+                    {
+                        carPos--; //move left
+                    }
+                    else if (key == 'd')
+                    {
+                        carPos++; // move right
+                    }
+                }
+
+                if (carPos + leftBoarder <= leftBoarder + 1 ||
+                    carPos + leftBoarder + 1 >= leftBoarder + 20 + 1) // if carPos is the same as the border
+                {
+                    Console.WriteLine($"CRASH! \n {rounds} Runden geschafft! \n Spiel wiederholen (Y/N)?");
+                    if (Console.ReadKey().KeyChar == 'y') //if retry
+                    {
+                        leftBoarder = 10; //reset vars
+                        carPos = 4;
+                        Console.Clear(); // clear console
+                    }
+                    else
+                    {
+                        retry = false;
+                    }
+                }
+
+                rounds++; // increment round
+                changeDirectionTimer--; // decrement Timer
+                System.Threading.Thread.Sleep(150); // Sleep
+            }
+        }
+    }
+}
---
+++ Übung/Übung/Übung04/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Linq;
+
+namespace Übung04
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            // define 2 sorted integer arrays
+            int[] a1 = {2, 4, 10, 20, 25};
+            int[] a2 = {1, 2, 3, 7, 10, 11, 20};
+ 
+// define a third integer array
+            int[] a3 = new int[a1.Length + a2.Length];
+ 
+// What does the rest of the code?
+            for (int i1 = 0, i2 = 0, i3 = 0, e1 = a1.Length, e2 = a2.Length, e3 = e1 + e2; i3 < e3; ++i3) {
+                a3 [i3] = i2 >= e2 || i1 < e1 && a1 [i1] < a2 [i2] ? a1 [i1++] : a2 [i2++];
+            }
+ 
+            Console.WriteLine (string.Join (" ", a3));
+            Aufgabe01("test");
+            Aufgabe02("testtset");
+            Aufgabe02("ghabfbafb");
+            Console.WriteLine($"10 Namen {string.Join(",",Aufgabe03())}");
+            int[,]test=Aufgabe09(3);
+            for (int i = 0; i < test.Rank-1; i++)
+            {
+                for (int j = 0; j < test.GetLength(i)-1; j++)
+                {
+                    Console.Write($"{test[i,j]},");
+
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void Aufgabe01(string eingabe)
+        {
+            for (int i = eingabe.Length - 1; i > -1; i--)
+            {
+                Console.Write(Convert.ToString(eingabe[i]));
+            }
+
+            Console.WriteLine();
+        }
+        static void Aufgabe02(string eingabe)
+        {
+            bool ispalindrom = false;
+            for (int i = 0; i < eingabe.Length / 2 - 1; i++)
+            {
+                if (eingabe[i] == eingabe[eingabe.Length - 1 - i])
+                {
+                    ispalindrom = true;
+                }
+                else
+                {
+                    ispalindrom = false;
+                    i = eingabe.Length;
+                }
+            }
+
+            Console.WriteLine($"ist {eingabe} ein Palindrom ? {ispalindrom}");
+        }
+
+        static string[] Aufgabe03()
+        {
+            return new string[] {"a","b","c"};
+        }
+
+        static int[,] Aufgabe09(int dim)
+        {
+            int[,] array = new int[dim,dim] ;
+            for (int i = 0; i < array.Rank-1; i++)
+            {
+                for (int j = 0; j < array.GetLength(i)-1; j++)
+                {
+                    array[i, j] = i * j;
+                }
+            }
+
+            return array;
+        }
+    }
+}
