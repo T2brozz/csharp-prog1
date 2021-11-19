4c463d544c7b887dc4f6df8721a36b849c18a643
T2brozz <42860763+T2brozz@users.noreply.github.com>
2021-11-19 20:31:45 +0100

    add praktikum 06 and übung06

---
+++ Praktikum06/Praktikum06/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace Praktikum06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (double d = -Math.PI; d < Math.PI; d+=0.3) 
+            { 
+                Console.WriteLine($"{Math.Sin(d),25} | {MySin(d),25} | {Math.Sin(d)-MySin(d),25}"); 
+            }
+            Console.WriteLine("-------------------------------"); 
+           
+            Console.WriteLine($"Wuerfeltest: längste Kette: {Wuerfeltest(10000)}"); 
+            // Die Anzahl bei funktionierendem Programm erhöhen! 
+ 
+            Console.WriteLine("-------------------------------"); 
+        }
+
+        static double MySin(double x)
+        {
+            double sin = 0.0;
+            for (int i = 0; i < 18; i++)
+            {
+                sin += Math.Pow(-1, i) * (double)(Math.Pow(x, 2 * i + 1) / (double) Fac(2 * i + 1));
+            }
+
+            return sin;
+        }
+
+        static long Fac(int num)
+        {
+            long fac = num;
+            for (int i = num - 1; i >= 1; i--)
+            {
+                fac = fac * i;
+            }
+            
+            return fac;
+        }
+        static int Wuerfeltest(int rounds)
+        {
+            int maxPasch = 0;
+            Random rand = new Random();
+            int num = rand.Next(6) + 1;
+            for (int i = 0; i < rounds; i++)
+            {
+                int paschRounds = PaschAnzahl(rand, ref num);
+                if ( paschRounds> maxPasch)
+                {
+                    maxPasch = paschRounds;
+                }
+            }
+
+            return maxPasch;
+        }
+
+        static int PaschAnzahl(Random wuerfel,ref int ersterWurf)
+        {
+            int rand;
+            int rounds = 0;
+            do
+            {
+                rand = wuerfel.Next(6) + 1;
+                rounds++;
+
+            } while (rand==ersterWurf);
+
+            ersterWurf = rand;
+            return rounds;
+        }
+        
+    }
+    
+}
---
+++ Übung/Übung/übung06/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Reflection.PortableExecutable;
+
+namespace übung06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            aufgabe02();
+        }
+
+        static void aufgabe01()
+        {
+            int[,] testarr = aufgabeA();
+            ausgabe(testarr);
+            Console.WriteLine();
+            ausgabe(aufgabeC(testarr));
+            testarr = aufgabeA();
+            Console.WriteLine("\n");
+            ausgabe(testarr);
+            Console.WriteLine("-----------");
+            Console.WriteLine(string.Join(",",aufgabed(testarr)));
+            
+            static int[,] aufgabeA()
+            {
+                int[,] arr = new int[3, 5];
+                Random rand = new Random();
+                for (int i = 0; i < arr.GetLength(0); i++)
+                {
+                    for (int j = 0; j < arr.GetLength(1); j++)
+                    {
+                        arr[i, j] = rand.Next(10);
+                    }
+                }
+
+                return arr;
+            }
+
+            static void ausgabe(int[,] ausgabearr)
+            {
+                for (int i = 0; i < ausgabearr.GetLength(0); i++)
+                {
+                    for (int j = 0; j < ausgabearr.GetLength(1); j++)
+                    {
+                        Console.Write(ausgabearr[i, j] + ",");
+                    }
+
+                    Console.WriteLine();
+                }
+            }
+
+            static int[,] aufgabeC(int[,] arr)
+            {
+                int[,] output = new int[arr.GetLength(1), arr.GetLength(0)];
+                for (int i = 0; i < arr.GetLength(0); i++)
+                {
+                    for (int j = 0; j < arr.GetLength(1); j++)
+                    {
+                        output[j, i] = arr[i, j];
+                    }
+                }
+
+                return output;
+            }
+
+            static int[] aufgabed(int[,] arr)
+            {
+                int[] sum = new int[arr.GetLength(1)];
+                for (int i = 0; i < arr.GetLength(0); i++)
+                {
+                    for (int j = 0; j < arr.GetLength(1); j++)
+                    {
+                        sum[j] += arr[i, j];
+                    }
+                }
+
+
+                return sum;
+            }
+        }
+
+        static void aufgabe02()
+        {
+            static int ZifferAnzahl(int num, int ziffer)
+            {
+                int numTemp = num;
+                int numCount = 0;
+                while (numTemp != 0)
+                {
+                    if (numTemp % 10 == ziffer)
+                    {
+                        numCount++;
+                    }
+
+                    numTemp /= 10;
+                }
+
+                return numCount;
+            }
+            int[] count = new int[10];
+            int seats = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i < seats+1; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    count[j] += ZifferAnzahl(i, j);
+                }
+            }
+            Console.WriteLine("1  2  3  4  5  6  7  8  9  0");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write(count[i]+" ");
+            }
+        }
+    }
+}
