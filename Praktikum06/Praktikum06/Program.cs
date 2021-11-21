4e42885778a727be085997982df8e760c4ffe695
T2brozz <42860763+T2brozz@users.noreply.github.com>
2021-11-21 13:58:52 +0100

    finished praktikum06

--- Praktikum06/Praktikum06/Program.cs
+++ Praktikum06/Praktikum06/Program.cs
@@ -6,6 +6,7 @@ namespace Praktikum06
     {
         static void Main(string[] args)
         {
+            //Test Code wie angegeben
             for (double d = -Math.PI; d < Math.PI; d+=0.3) 
             { 
                 Console.WriteLine($"{Math.Sin(d),25} | {MySin(d),25} | {Math.Sin(d)-MySin(d),25}"); 
@@ -13,64 +14,105 @@ namespace Praktikum06
             Console.WriteLine("-------------------------------"); 
            
             Console.WriteLine($"Wuerfeltest: längste Kette: {Wuerfeltest(10000)}"); 
-            // Die Anzahl bei funktionierendem Programm erhöhen! 
+            
  
+            Console.WriteLine("---------------------- ---------"); 
+            int a = 5, b = 3; 
+            SortSwap(/* ... */ ref a, /* ... */ ref b); 
+            Console.WriteLine($"Sort-Swap: {a} - {b}"); 
+            SortSwap(/* ... */ ref a, /* ... */ ref b); 
+            Console.WriteLine($"Sort-Swap: {a} - {b}");
             Console.WriteLine("-------------------------------"); 
+            double g, f; 
+            DoubleSplit(3.14, /* ... */  out g, /* ... */  out f); 
+            Console.WriteLine($"Double-Split: {g}|{f}");
         }
 
+        //Übergabe durch return da aufgabe einen RückgabeWert angibt
         static double MySin(double x)
         {
-            double sin = 0.0;
+            //sinus Wert
+            double sin = 0;
+            //Tylor Reihe itteration
             for (int i = 0; i < 18; i++)
             {
-                sin += Math.Pow(-1, i) * (double)(Math.Pow(x, 2 * i + 1) / (double) Fac(2 * i + 1));
+                //Formel für Berechung jedes Gliedes und addiere es zum Sinuswert
+                sin += Math.Pow(-1, i) *(Math.Pow(x, 2 * i + 1) / Fak(2 * i + 1));
             }
 
             return sin;
         }
 
-        static long Fac(int num)
+    
+        //gibt falkultät von num zurück
+        static double Fak(int num)
         {
-            long fac = num;
+            //zwischenspeicher von num
+            double fac = num;
             for (int i = num - 1; i >= 1; i--)
             {
-                fac = fac * i;
+                fac = fac * i; // multipliziert elment auf fac
             }
             
-            return fac;
+            return fac; 
         }
+        //  return , weil in aufgabe angegeben und  funktioniert für test main 
         static int Wuerfeltest(int rounds)
         {
+            //speichert längsten Pasch
             int maxPasch = 0;
+            //speichert rand für wiederbenutzung
             Random rand = new Random();
+            //generiert 1 zahl ziwschen 1 und 6 
             int num = rand.Next(6) + 1;
             for (int i = 0; i < rounds; i++)
             {
+                //bekommt paschlänge in diesem versuch
                 int paschRounds = PaschAnzahl(rand, ref num);
-                if ( paschRounds> maxPasch)
+                if ( paschRounds> maxPasch) //schaut ob paschlänge größer als der längste pasch ist
                 {
-                    maxPasch = paschRounds;
+                    maxPasch = paschRounds; // überschreibt diesen
                 }
             }
 
             return maxPasch;
         }
 
+        //es sollen 2 werte zurückgegeben werden. Einer durch return und der andere als ref 
         static int PaschAnzahl(Random wuerfel,ref int ersterWurf)
         {
-            int rand;
-            int rounds = 0;
+            int rand; //zufällige Zhal
+            int rounds = 0; // counter der die runden speichert
             do
             {
-                rand = wuerfel.Next(6) + 1;
-                rounds++;
+                rand = wuerfel.Next(6) + 1; // generiert neue Zahl
+                rounds++; 
 
-            } while (rand==ersterWurf);
+            } while (rand==ersterWurf); // wiederholt solange bis nichtmer num gewürfelt wurde
 
-            ersterWurf = rand;
+            ersterWurf = rand; 
             return rounds;
         }
-        
+
+        // nutzung von ref , weil a und b in der main schon initalisiert sind 
+
+        static void SortSwap(ref int a, ref int b)
+        {
+            //wenn a > b dann müssen beide werte getaust werden
+            if (a>b)
+            {
+                int tmp = a; // tmp variable speichert a
+                a = b; //a bekommt den wert von b 
+                b = tmp; // b bekommt den wert von der in tmp gespeicht ist
+            }
+        }
+
+        //nutzt out,da die variablen in der main noch keine werte zugewiesen bekommen haben
+        static void DoubleSplit(double d, out double beforeKomma, out double afterKomma)
+        {
+            beforeKomma = (int)d; // nimmt teil vor dem komma durchs casten
+            afterKomma = d-beforeKomma; // zieht ganzzahligen teil ab um die komma zahl zu bekommen 
+        }
     }
     
 }
